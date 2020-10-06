f7f15d29190baf5edad86ef25a1903fe4805a317
ExLog <muhammad.doni2004@gmail.com>
2020-10-06 23:07:35 +0800

    Changed channel id

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 token = "NzYxMjM0NjE1MzcyNzQyNjU2.X3Xo-w.u6FCFMfiTNonMf-7yi1yQesaisk"
-channel_id = 761972711551270952
+channel_id = 728879581410361405
 prefix = "eve "
 
 client = commands.Bot(prefix)
